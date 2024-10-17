e62c3aa616c876b355b72ada4eadc1e46e17f240
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2024-10-17 08:58:35 +0000

    modified file keeps the same name as original now

--- fastq_prep/change_sequence_identifiers.sh
+++ fastq_prep/change_sequence_identifiers.sh
@@ -12,7 +12,7 @@ for file in $folder/*_1.fastq.gz; do
     # Modify the content: replace " " with "/1" and remove everything after the white space
     awk '{sub(/ .*/, "/1", $0); print}' |
     # Compress the modified content and save to new file with mod_ prefix
-    gzip > "mod_${base_name}.fastq.gz"
+    gzip > "$folder/${base_name}.fastq.gz"
 
     echo "Processed: $folder/${base_name}.fastq.gz"
 done
@@ -28,7 +28,7 @@ for file in $folder/*_2.fastq.gz; do
     # Modify the content: replace " " with "/2" and remove everything after the white space
     awk '{sub(/ .*/, "/2", $0); print}' |
     # Compress the modified content and save to new file with mod_ prefix
-    gzip > "mod_${base_name}.fastq.gz"
+    gzip > "$folder/${base_name}.fastq.gz"
 
     echo "Processed: $folder/${base_name}.fastq.gz"
 done
